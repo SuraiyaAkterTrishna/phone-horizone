5f95866d83eb2d5a0885d812b55dd3bb65597c46
SuraiyaAkterTrishna <suraiyaaktertrishna@gmail.com>
2022-03-02 05:41:53 +0600

    search result done

--- js/script.js
+++ js/script.js
@@ -3,6 +3,8 @@ const searchPhone = () => {
    const searchField = document.getElementById('search-field');
    const searchText = searchField.value;
    searchField.value = '';
+   const phoneDetails = document.getElementById('phone-details');
+    phoneDetails.textContent = '';
    const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
    fetch(url)
    .then(res => res.json())
@@ -16,22 +18,67 @@ const displaySearchResult = phones => {
         const div = document.createElement('div');
         div.classList.add('col');
         div.innerHTML = `
-        <div class="card">
+        <div class="card h-100">
             <img src="${phone.image}" class="mt-3 mx-auto d-block w-75" alt="phone-image">
             <div class="card-body mx-5">
                 <h5 class="card-title">${phone.phone_name}</h5>
                 <p class="card-text">${phone.brand}</p>
             </div>
-            <button onclick="loadPhoneDetail('${phone.slug}')" class="btn btn-outline-success w-75 mx-auto m-3">Explore</button>
+            <button onclick="loadPhoneDetails('${phone.slug}')" class="btn btn-outline-success w-75 mx-auto m-3">Explore</button>
         </div>
         `;
         searchResult.appendChild(div);
     });
 }
-const loadPhoneDetail = id => {
-    console.log(id);
+//single phone details area
+const loadPhoneDetails = id => {
     const url = `https://openapi.programming-hero.com/api/phone/${id}`;
     fetch(url)
     .then(res => res.json())
-    .then(data => console.log(data.data))
+    .then(data => displayPhoneDetails(data.data))
+}
+// display phone details 
+const displayPhoneDetails = phone => {
+    const phoneDetails = document.getElementById('phone-details');
+    phoneDetails.textContent = '';
+    const div = document.createElement('div');
+    div.classList.add('card');
+    div.innerHTML = `
+    <img src="${phone.image}" class="mt-3 mx-auto d-block" alt="phone-image">
+    <div class="card-body table-responsive">
+    <table class="table table-bordered table-hover">
+    <tbody>
+      <tr>
+        <th scope="row">Name:</th>
+        <td>${phone.name}</td>
+      </tr>
+      <tr>
+        <th scope="row">Release Date:</th>
+        <td>${phone.releaseDate?phone.releaseDate : 'No releade date found'}</td>
+      </tr>
+      <tr>
+        <th scope="row">Chipset:</th>
+        <td>${phone.mainFeatures.chipSet}</td>
+      </tr>
+      <tr>
+        <th scope="row">Display Size:</th>
+        <td>${phone.mainFeatures.displaySize}</td>
+      </tr>
+      <tr>
+        <th scope="row">Memory:</th>
+        <td>${phone.mainFeatures.memory}</td>
+      </tr>
+      <tr>
+        <th scope="row">Sensors:</th>
+        <td colspan="2" class="horizontal-align: middle">${phone.mainFeatures.sensors}</td>
+      </tr>
+      <tr>
+        <th scope="row">Storage:</th>
+        <td>${phone.mainFeatures.storage}</td>
+      </tr>
+    </tbody>
+  </table>
+    </div>`;
+
+    phoneDetails.appendChild(div);
 }
