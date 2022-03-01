66443f969fe11b25d999164bc61fba7aa8b70fdb
SuraiyaAkterTrishna <suraiyaaktertrishna@gmail.com>
2022-03-02 00:19:59 +0600

    card using from bootstrap

--- js/script.js
+++ js/script.js
@@ -5,6 +5,13 @@ const searchPhone = () => {
    const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
    fetch(url)
    .then(res => res.json())
-   .then(data => console.log(data));
-
+   .then(data => displaySearchResult(data.data));
+}
+const displaySearchResult = phones => {
+    const searchResult = document.getElementById('search-result');
+    phones.forEach(phone => {
+        console.log(phone);
+        const div = document.createElement('div');
+        div.classList.add('col');
+    });
 }
