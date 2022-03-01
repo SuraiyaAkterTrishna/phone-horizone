a1df9afa297bdb886c8b25e54cf3bf6203f29f9b
SuraiyaAkterTrishna <suraiyaaktertrishna@gmail.com>
2022-03-02 01:16:30 +0600

    phone search area done

--- js/script.js
+++ js/script.js
@@ -5,13 +5,24 @@ const searchPhone = () => {
    const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
    fetch(url)
    .then(res => res.json())
-   .then(data => displaySearchResult(data.data));
+   .then(data => displaySearchResult(data.data.slice(0,20)));
 }
 const displaySearchResult = phones => {
     const searchResult = document.getElementById('search-result');
+    searchResult.textContent = '';
     phones.forEach(phone => {
         console.log(phone);
         const div = document.createElement('div');
         div.classList.add('col');
+        div.innerHTML = `
+        <div class="card">
+            <img src="${phone.image}" class="mt-3 mx-auto d-block w-75" alt="phone-image">
+            <div class="card-body mx-5">
+                <h5 class="card-title">${phone.phone_name}</h5>
+                <p class="card-text">${phone.brand}</p>
+            </div>
+        </div>
+        `;
+        searchResult.appendChild(div);
     });
 }
